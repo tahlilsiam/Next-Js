7b9af8836165bdc39d963dd6c041a0a5840f3af6
Tahlil Abrar Siaam <111914265+tahlilsiam@users.noreply.github.com>
2024-02-07 10:34:57 +0000

    Some styling in Navbar

---
+++ demo-next-app/app/Issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IssuesPage = () => {
+  return (
+    <div>
+      This is an issues
+    </div>
+  )
+}
+
+export default IssuesPage
--- demo-next-app/app/Navbar.tsx
+++ demo-next-app/app/Navbar.tsx
@@ -1,18 +1,36 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { FaFileArchive } from "react-icons/fa";
+import classnames from 'classnames';
 
 
 const Navbar = () => {
+    const currentPath = usePathname();
+    console.log(currentPath);
+
     const linkList =[
         {label: 'Dashboard', href:'/'},
-        {label: 'Issues', href:'/issues'}
+        {label: 'Issues', href:'/Issues'}
     ]
   return (
     <nav className='flex space-x-6 border-b mb-5 px-4 h-14 items-center'>
       <Link href="/"><FaFileArchive /></Link>
       <ul className='flex space-x-6 '>
-        {linkList.map((links)=><Link key={links.href} className='text-blue-500 hover:text-blue-800 transition-colors' href={links.href}>{links.label}</Link>)}
+        {linkList.map((links)=>
+        <Link key={links.href} 
+        className={classnames(
+            'text-green-500',
+            {
+                underline: links.href === currentPath,
+                'hover:text-green-700 transition-colors': true
+            }
+        )}
+        href={links.href}>
+            {links.label}
+        </Link>)}
       </ul>
     </nav>
   )
